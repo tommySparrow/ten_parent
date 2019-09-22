1c391f393231bb8835997d7e54fe1dd4529a97dc
jm90_yang <jm90_yang@163.com>
2019-09-22 16:59:31 +0800

    springcache 的使用 及ten_gathering 模块

---
+++ ten_gathering/src/main/java/com/ten/gathering/GatheringApplication.java
@@ -0,0 +1,24 @@
+package com.ten.gathering;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import util.IdWorker;
+
+/**
+ * @ EnableCaching 使用spring boot的缓存
+ */
+@EnableCaching
+@SpringBootApplication
+public class GatheringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatheringApplication.class, args);
+	}
+
+	@Bean
+	public IdWorker idWorkker(){
+		return new IdWorker(1, 1);
+	}
+	
+}
